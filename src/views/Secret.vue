59ff7ab4e10ae413301e024ddfd78b5b189307b2
LaG111 <2969373627@qq.com>
2021-11-30 10:50:46 +0800

    zancun

--- src/views/Secret.vue
+++ src/views/Secret.vue
@@ -7,27 +7,46 @@
 		<v-btn @click="upload">
 			点击上传图片	
 		</v-btn>
-		<input
+		<form ref ='form' style="display :none">
+			<input
 		type="file"
 		name="file"
 		ref ="file"
-		@change="changeImg($event.target.files[0])"
+		@change="changeImg($event.target.files)"
 		accept="image/gif, image/jpeg , image/png"
 		style="display :none"/>
-
-		<v-img :src="img" alt="" style = 'z-index: index 100;'> </v-img>
+		</form>
+		
     </div>
+		<v-dialog   v-model="showCropper" persistent>
+
+			<div style="" class="pa -2">
+				<img id='img' ref='img' :src="img" alt="" style="display:block; max-width: 100%; width:200px" v-if="imgToggle">
+			</div>
+			<v-row>
+				<v-col>
+					<v-btn @click="imgToggle= false;showCropper =false;">关闭</v-btn>
+				</v-col>
+				<v-col>
+					<v-btn primary @click="imgToggle= false;showCropper =false;">确认</v-btn>
+				</v-col>
+			</v-row>
+		</v-dialog>
+
   </div>
 </template>
 
 <script>
+import Cropper from 'cropperjs'
+import 'cropperjs/dist/cropper.css';
 export default{
 	name:'secret',
 	data(){
 		return{
 			img:'',
 			croppedImg: '',
-
+			showCropper:false,
+			imgToggle:false
 		}
 	},
 	methods:{
@@ -35,25 +54,53 @@ export default{
 			//触发原生input元素的点击事件
            	this.$refs.file.click()
 		},
-		changeImg(file){
-			if(!this.check(file)){
-				this.$msg.info('兄啊，上传的图片作为头像也太大了吧？')
+		async changeImg(files){
+			if(!this.check(files[0])){
+				this.$msg.info('兄啊，上传的图片也太大了吧？')
 			}else{
+					this.img = await this.readToBase64(files[0])
+					this.cropTrigger()
+				}
+		},
+		check(file){
+			console.log(file)
+			if(file.size > 10240000){
+				return false
+			}
+			else{
+				return true
+			}
+		},
+		cropTrigger(){
+			this.showCropper =true
+			this.imgToggle = true
+			this.$nextTick(()=>{
+				let img = this.$refs.img
+				console.log('===========')
+				console.log(img)
+				let cropper = new Cropper(img,{
+					aspectRatio:  1 / 1,
+					viewMode :2,
+					dragMode : 'move'
+					})
+			})
+		},
+		readToBase64(file){
+			return new Promise((resolve,reject)=>{
 				let reader  = new FileReader
 				reader.readAsDataURL(file)
 				reader.onload = ()=>{
-					this.img = reader.result    
+					resolve(reader.result)
 				}
-				cropTrigger()
-			}
+			})
 		},
-		check(file){
-			if(file.size > 10240000)
-				return false
-		},
-		cropTrigger(){
-			
-		},
+	},
+	components:{
 	}
 }
 </script>
+
+<style scoped>
+
+	
+</style>
